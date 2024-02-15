e9d4b568e319d4cb7ae2cad2bae67052db7fee38
AlexVCaron <alex.valcourt.caron@usherbrooke.ca>
2024-02-15 06:18:01 +0000

    update package creator

--- create_package.sh
+++ create_package.sh
@@ -10,12 +10,13 @@ if [ ! -d "store/$VENDOR" ]
 then
 
 mkdir store/$VENDOR
-cat <<- EOF > store/$VENDOR/README.md
-CREATED BY NEUROGISTRY
+cat <<- EOF > store/$VENDOR/meta.yml
+description: CREATED BY NEUROGISTRY
+packages:
 EOF
 
-git add store/${VENDOR}/README.md
-git commit -m "Neurogister added $VENDOR"
+git add $VENDOR/meta.yml
+git commit -m "Added vendor $VENDOR"
 
 fi
 
@@ -28,9 +29,17 @@ fi
 mkdir -p store/$VENDOR/$PACKAGE
 for file in $@
 do
-    ln -s $file store/$VENDOR/$PACKAGE/$(basename $file)
+    ln -s $PWD/$file store/$VENDOR/$PACKAGE/$(basename $file)
 done
 
+cat <<- EOF >> store/$VENDOR/meta.yml
+  - $PACKAGE
+EOF
+
+git add $VENDOR/meta.yml
+
 dvc add --no-commit store/$VENDOR/$PACKAGE
-#dvc commit -f store/$VENDOR/$PACKAGE
-#dvc push store/$VENDOR/$PACKAGE
+dvc commit -f store/$VENDOR/$PACKAGE
+dvc push store/$VENDOR/$PACKAGE
+
+git commit -m "Create package $PACKAGE for vendor $VENDOR"
